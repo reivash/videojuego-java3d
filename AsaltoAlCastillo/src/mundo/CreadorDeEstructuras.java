745c18560b251d04f1e14765935c6990e481704d
Nur AG <nurloco@hotmail.com>
2014-06-03 19:50:27 +0200

    Añadido el creador de torres.
    Hay que comprobar que funcionen los ángulos porque no estoy seguro de
    haber puesto senos y cosenos bien.
    
    Yakarta.

--- AsaltoAlCastillo/src/mundo/CreadorDeEstructuras.java
+++ AsaltoAlCastillo/src/mundo/CreadorDeEstructuras.java
@@ -1,23 +1,23 @@
 package mundo;
 
 import javax.media.j3d.BranchGroup;
-import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
 
 public class CreadorDeEstructuras {
 
     /* Se han de crear entidades fisicas y meterlas al branchgroup */
     /* Los tamaños se especifican en mitades */
-    public static void crearBloque(Vector3d centro, Vector3d tamaños, Vector3d rotacion, BranchGroup conjunto) {
+    public static void crearBloque(Vector3f centro, Vector3f tamaños, Vector3f rotacion, BranchGroup conjunto) {
         /* ToDo: Implementar */
     }
     
     /* Los muros tienen un ancho prefijado */
     public static void crearMuro(
-            Vector3d comienzo, 
-            Vector3d fin,
+            Vector3f comienzo, 
+            Vector3f fin,
             float altura,
             int numeroBloques,
-            Vector3d rotacion,
+            Vector3f rotacion,
             BranchGroup conjunto) 
     {
         /* ToDo: Implementar */
@@ -25,13 +25,20 @@ public class CreadorDeEstructuras {
     
     /* Creación de una torre cilíndrica */
     public static void crearTorre(
-            Vector3d pos,
-            float altura,
+            Vector3f pos,
+            float radio,
+            int altura,
             int numeroBloques,
-            Vector3d rotacion,
             BranchGroup conjunto) 
     {
-        /* ToDo: Implementar */
+        for(int i = 0; i < altura; i++){
+            float angDesplazamiento = (float) (Math.PI/numeroBloques); 
+            for(int j = 0; j < numeroBloques; j++){
+                 float angulo = (float) (j==0?0:(2*Math.PI/j + (j&1)==0?0:angDesplazamiento));
+                 Vector3f posCentro = new Vector3f((float) (pos.x+radio*(Math.sin(angulo))), pos.y + i, (float) (pos.x+radio*(Math.cos(angulo))));
+                 crearBloque(posCentro, new Vector3f(2,1,1), new Vector3f(0,angulo,0), conjunto);
+            }
+        }
     }
     
 }
