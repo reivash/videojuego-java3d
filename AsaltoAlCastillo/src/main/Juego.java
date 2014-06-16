f438928cfd2b28971ec4c2b39c4c760d66210514
Nur AG <nurloco@hotmail.com>
2014-06-16 14:15:21 +0200

    Bola para Sanjay.
    Que le gustan las bolitas
    .

--- AsaltoAlCastillo/src/main/Juego.java
+++ AsaltoAlCastillo/src/main/Juego.java
@@ -218,8 +218,8 @@ public class Juego extends JFrame {
 //        CreadorDeEstructuras.crearMuro(new Vector3f(200, 0, 400 - radioTorre / 2 - offset * 1.4f), new Vector3f(100 , 0f, 150 + radioTorre / 2 + offset * 1.4f), alturaMuros, numPiezas, conjunto, this);
 
         /* Prueba de bola */
-//        entidad.Bola b = new entidad.Bola(1, 64, new Vector3f(-750000,500,0), "res//texturas//bola.jpg", conjunto, this);
-//        b.crearPropiedades(100, 0, dampingLineal, new Vector3f(0,0,1), new Vector3f());
+        entidad.Bola b = new entidad.Bola(1, 64, new Vector3f(-750000,500,0), "res//texturas//bola.jpg", conjunto, this);
+        b.crearPropiedades(100, 0, dampingLineal, new Vector3f(0,0,1), new Vector3f());
         /* Test de rotación de muros */
 //        for (int i = 0; i < 8; i++) {
 //            CreadorDeEstructuras.crearMuro(new Vector3f(-30f + i*10, 0f, 20f), new Vector3f(-300f + i*100f, 0f, 400f), 10, 5, conjunto, this);
