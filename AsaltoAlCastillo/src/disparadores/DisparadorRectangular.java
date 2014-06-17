b2babe8482f0c3f86d8c286022cea41b6c123e1a
KayHub <j.c.revolution@hotmail.com>
2014-06-17 21:06:55 +0200

    Disparador rectangular visible

--- AsaltoAlCastillo/src/disparadores/DisparadorRectangular.java
+++ AsaltoAlCastillo/src/disparadores/DisparadorRectangular.java
@@ -1,9 +1,19 @@
 package disparadores;
 
+import com.sun.j3d.utils.geometry.Box;
+import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.image.TextureLoader;
 import entidad.DiccionarioEntidades;
 import entidad.Entidad;
 import eventos.Evento;
 import java.util.List;
+import javax.media.j3d.Appearance;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Texture;
+import javax.media.j3d.TextureAttributes;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Vector3f;
 import main.Juego;
 
 /* 
@@ -14,15 +24,15 @@ public class DisparadorRectangular extends DisparadorAbstracto {
     private DiccionarioEntidades dE = DiccionarioEntidades.getInstance();
 
     private float x1, z1, x2, z2;
-    
+
     private Juego juego = null;
     private Evento e = null;
 
-    public DisparadorRectangular(List<String> objetivos, Juego juego, Evento e, float x1, float z1, float x2, float z2) {
+    public DisparadorRectangular(List<String> objetivos, Juego juego, Evento e, float x1, float z1, float x2, float z2, BranchGroup bg) {
         super(objetivos);
         this.juego = juego;
         this.e = e;
-        
+
         /* x1 siempre será el mínimo de los dos x. z1 siempre será el mínimo de los dos z */
         if (x1 < x2) {
             this.x1 = x1;
@@ -37,6 +47,25 @@ public class DisparadorRectangular extends DisparadorAbstracto {
         } else {
             this.z1 = z2;
             this.z2 = z1;
+        }
+        if (bg != null) {
+            BranchGroup boundsBG = new BranchGroup();
+            Appearance ap = new Appearance();
+            Texture tex = new TextureLoader("res//texturas//cespedfutbol.jpg", null).getTexture();
+            ap.setTexture(tex);
+            TextureAttributes texAttr = new TextureAttributes();
+            texAttr.setTextureMode(TextureAttributes.MODULATE);
+            ap.setTextureAttributes(texAttr);
+
+            Box figuraVisual = new Box((this.x2 - this.x1)/2, .05f, (this.z2 - this.z1)/2, 
+                    Box.GENERATE_TEXTURE_COORDS, ap);
+            Transform3D t3D = new Transform3D();
+            t3D.setTranslation(new Vector3f((x1 + x2) / 2, -2, (z1 + z2) / 2));
+            TransformGroup desplazamiento = new TransformGroup();
+            desplazamiento.setTransform(t3D);
+            desplazamiento.addChild(figuraVisual);
+            boundsBG.addChild(desplazamiento);
+            bg.addChild(boundsBG);
         }
         dE.añadirDisparador(this);
     }
@@ -57,8 +86,8 @@ public class DisparadorRectangular extends DisparadorAbstracto {
                 b = true;
                 continue;
             }
-            x = 
-                    ent.posiciones[0];
+            x
+                    = ent.posiciones[0];
             z = ent.posiciones[2];
             if (x1 <= x && x <= x2
                     && z1 <= z && z <= z2) {
